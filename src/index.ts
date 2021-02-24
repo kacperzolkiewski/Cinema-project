84e0d441ffe2c46b11e889892002f5f969bf7584
Karolina Gutka <karolina.gutka.97@gmail.com>
2021-02-24 22:49:05 +0100

    Generate QR Code Image example

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import express from "express";
-const app = express();
-const port = process.env.PORT || 8080;
+import express from "express"
+import QRCodeService from "./services/qrCode/generateqrcode"
+const app = express()
+const port = process.env.PORT || 8080
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+    res.send("Hello World!")
+})
+QRCodeService("Example data")
 
-app.listen(port);
+app.listen(port)
