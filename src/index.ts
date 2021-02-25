d8080197ca2be2c102772962fce199743931d3c3
Karolina Gutka <karolina.gutka.97@gmail.com>
2021-02-25 21:34:59 +0100

    Create connection with MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-import express from "express";
-const app = express();
-const port = process.env.PORT || 8080;
+import express from "express"
+import mongoose from "mongoose"
+import "dotenv/config"
+
+const app = express()
+const port = process.env.PORT || 8080
+
+const connectMongoDB = async (): Promise<void> => {
+    const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.u2bdz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    await mongoose
+        .connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(
+            () => console.log("MongoDB connected"),
+            (error) => {
+                console.error(error)
+            },
+        )
+}
+
+connectMongoDB()
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+    res.send("Hello World!")
+})
 
-app.listen(port);
+app.listen(port)
