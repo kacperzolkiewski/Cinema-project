3f8162d2b531ce095cbc4bb024879641a443f9d7
Karolina Gutka <74671633+kgutka@users.noreply.github.com>
2021-03-01 11:05:01 +0100

    Create connection with MongoDB (#15)
    * Create connection with MongoDB
    
    * Move mongoDB link to .env, remove unnecessary git add from package.json

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-import express from "express";
-const app = express();
-const port = process.env.PORT || 8080;
+import express from "express"
+import mongoose from "mongoose"
+import "dotenv/config"
+
+const app = express()
+const port = process.env.PORT || 8080
+
+const connectMongoDB = async (): Promise<void> => {
+    const uri = process.env.MONGODB_URI
+    await mongoose
+        .connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(
+            () => console.log("MongoDB connected"),
+            (error) => {
+                console.error(error)
+            },
+        )
+}
+
+connectMongoDB()
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+    res.send("Hello World!")
+})
 
-app.listen(port);
+app.listen(port)
