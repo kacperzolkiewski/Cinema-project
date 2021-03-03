383a3d3d643061ac74ed9411b00e321ff5469352
Mateusz Cabała <mateusz.cabala95@gmail.com>
2021-03-03 20:01:19 +0100

    Add function which print HTML to PDF file

---
+++ src/utils/PdfGenerator/PdfPrinter.ts
@@ -0,0 +1,23 @@
+import * as puppeteer from "puppeteer"
+
+const PdfPrinter = async (HTMLToPrint: any): Promise<void> => {
+    try {
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+
+        await page.setContent(HTMLToPrint)
+        await page.emulateMediaType("print")
+
+        await page.pdf({
+            path: "ticket.pdf",
+            format: "a4",
+            printBackground: true,
+        })
+        await browser.close()
+        process.exit()
+    } catch (e) {
+        throw new Error(e.message)
+    }
+}
+
+export default PdfPrinter
