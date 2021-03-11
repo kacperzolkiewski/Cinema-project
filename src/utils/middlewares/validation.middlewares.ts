214eee093f2ec57e18a8865000bae84eb42d4afa
Mateusz Cabała <mateusz.cabala95@gmail.com>
2021-03-11 19:11:51 +0100

    Add validation middleware

---
+++ src/utils/middlewares/validation.middlewares.ts
@@ -0,0 +1,21 @@
+import {plainToClass} from "class-transformer";
+import {validate, ValidationError} from "class-validator";
+import {Request, RequestHandler, Response, NextFunction} from "express";
+
+function validationMiddleware<T> (type: any, skipMissingProperties = false): RequestHandler  {
+    return ((req: Request, res: Response, next: NextFunction) =>{
+        validate(plainToClass(type, req.body), {skipMissingProperties})
+            .then((errors: ValidationError[]) =>{
+                if (errors.length > 0){
+                    res.status(400).json({
+                        error: errors,
+                        data: req.body
+                    })
+                } else{
+                    next()
+                }
+        })
+    } )
+}
+
+export default validationMiddleware;
