9fc07346f58f69c1cbcc8cd51706ae3368844e91
Mateusz Cabała <mateusz.cabala95@gmail.com>
2021-03-11 21:45:46 +0100

    update http status and params type

--- src/controllers/filmController/film.controller.ts
+++ src/controllers/filmController/film.controller.ts
@@ -5,15 +5,17 @@ import FilmDTO from "./Film.dto"
 
 const router = Router()
 
+type RequestParam = { id?: string }
+
 router.get("/", async (req: Request, res: Response) => {
   await FilmModel.find()
     .then((resp) => {
       res.status(200).json(resp)
     })
-    .catch((e) => res.status(500).json({ error: e.message }))
+    .catch((e) => res.status(503).json({ error: e.message }))
 })
 
-router.get("/:id", async (req: Request, res: Response) => {
+router.get("/:id", async (req: Request<RequestParam>, res: Response) => {
   const id = req.params.id
 
   await FilmModel.findById(id)
@@ -21,36 +23,36 @@ router.get("/:id", async (req: Request, res: Response) => {
       if (resp) {
         res.status(200).json(resp)
       } else {
-        res.status(404).json({ message: "Film with this ID" })
+        res.status(404).json({ message: "Film with given ID is not found" })
       }
     })
-    .catch((e) => res.status(500).json({ error: e.message }))
+    .catch((e) => res.status(503).json({ error: e.message }))
 })
 
-router.post("/", [validationMiddleware(FilmDTO)], async (req: Request, res: Response) => {
+router.post("/", [validationMiddleware(FilmDTO)], async (req: Request<RequestParam, {}, FilmDTO>, res: Response) => {
   await FilmModel.create(req.body)
     .then((resp) => {
       res.status(201).json(resp)
     })
-    .catch((e) => res.status(500).json({ error: e.message }))
+    .catch((e) => res.status(503).json({ error: e.message }))
 })
 
-router.patch("/:id", [validationMiddleware(FilmDTO)], async (req: Request<{ id?: string }, {}, FilmDTO>, res: Response) => {
+router.patch("/:id", [validationMiddleware(FilmDTO)], async (req: Request<RequestParam, {}, FilmDTO>, res: Response) => {
   const id = req.params.id
 
   await FilmModel.findByIdAndUpdate(id, req.body)
     .then((resp) => {
       res.status(200).json(resp)
     })
-    .catch((e) => res.status(500).json({ error: e.message }))
+    .catch((e) => res.status(503).json({ error: e.message }))
 })
 
-router.delete("/:id", async (req: Request, res: Response) => {
+router.delete("/:id", async (req: Request<RequestParam>, res: Response) => {
   const id = req.params.id
 
   await FilmModel.findByIdAndDelete(id)
     .then((resp) => res.status(200).json(resp))
-    .catch((e) => res.status(500).json(e.message))
+    .catch((e) => res.status(503).json(e.message))
 })
 
 export default router
