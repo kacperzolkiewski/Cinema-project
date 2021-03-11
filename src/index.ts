9360a8e9a6de1cfdb80549dba40f5100fa9515ec
Kuba <k.kopyto@cypher.dog>
2021-03-11 19:48:27 +0100

    it works

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,37 @@ import "dotenv/config"
 import pino from "pino"
 import expressPino from "express-pino-logger"
 import swaggerUi from "swagger-ui-express"
-import swaggerJsDoc from "swagger-jsdoc"
 import test from "./routes/test"
+
+const swaggerDocument = {
+    "swagger": "2.0",
+    "info": {
+        "title": "Cinema App",
+        "description": "An website app for cinema",
+        "version": "1.0"
+    },
+    "produces": ["application/json"],
+    "paths": {
+        "/test": {
+            "get": {
+                "tags": ["/test"],
+                "description": "Taki fajny description",
+                "parameters": [{
+                    "name": "test",
+                    "in": "formData",
+                    "type": "array",
+                    "collectionFormat": "multi",
+                    "items": {
+                        "type": "integer"
+                    }
+                },
+                { "name": "profileId", "in": "formData", "required": true, "type": "string" },
+                { "name": "file", "in": "formData", "type": "file", "required": "true" }],
+                "responses": {}
+            }
+        }
+    }
+}
 
 const logger = pino({ level: process.env.LOG_LEVEL || "info" })
 const expressLogger = expressPino({ logger })
@@ -13,20 +42,7 @@ const expressLogger = expressPino({ logger })
 const app = express()
 const port = process.env.PORT || 8080
 
-const swaggerDefinition = {
-    info: {
-        title: "Cinema App",
-        version: "1.0.0",
-    },
-}
-
-const options = {
-    swaggerDefinition,
-    apis: ["./routes/test.js"],
-}
-const swaggerSpec = swaggerJsDoc(options)
-
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(expressLogger)
 
