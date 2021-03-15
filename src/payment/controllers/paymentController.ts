54b1358c0e0e45356197d75cb82c6fb48c57f941
adam-kostuch <adamkostuch.krakow@gmail.com>
2021-03-15 23:04:37 +0100

    Rebasing 2/4

--- src/payment/controllers/paymentController.ts
+++ src/payment/controllers/paymentController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
-import paypal from "paypal-rest-sdk"
+import paypal, { SDKError, Payment } from "paypal-rest-sdk"
 
 paypal.configure({
     mode: "sandbox",
@@ -7,8 +7,49 @@ paypal.configure({
     client_secret: "EESgAZabiTtNvXCZNBJLUYntYGr4gHZfNf2-o82dQ6jmoYWUuj2mqy1kSvQ-NxnwW-1Eq75A6oHEoigQ",
 })
 
-export const createPayment = async (req: Request, res: Response) => {}
+export const createPayment = (req: Request, res: Response): void => {
+    const sample_payment_json: Payment = {
+        intent: "sale",
+        payer: {
+            payment_method: "paypal",
+        },
+        redirect_urls: {
+            return_url: "http://localhost:3000/success",
+            cancel_url: "http://localhost:3000/cancel",
+        },
+        transactions: [
+            {
+                item_list: {
+                    items: [
+                        {
+                            name: "Red Sox Hat",
+                            sku: "001",
+                            price: "25.00",
+                            currency: "USD",
+                            quantity: 1,
+                        },
+                    ],
+                },
+                amount: {
+                    currency: "USD",
+                    total: "25.00",
+                },
+                description: "Hat for the best team ever",
+            },
+        ],
+    }
 
-export const successPayment = async (req: Request, res: Response) => {}
+    paypal.payment.create(sample_payment_json, (error: SDKError, payment: Payment): void => {
+        if (error) {
+            throw error
+        } else {
+            payment.links!.forEach((link: paypal.Link) => {
+                link.rel === "approval_url" && res.redirect(link.href)
+            })
+        }
+    })
+}
 
-export const refundPayment = async (req: Request, res: Response) => {}
+// export const successPayment = (req: Request, res: Response): void => {}
+
+// export const refundPayment = (req: Request, res: Response): void => {}
