e79422572256a36d4aeae779c36b25e86e8f1d74
Kuba <k.kopyto@cypher.dog>
2021-03-16 18:54:09 +0100

    Hotfix

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import App from "./App"
 import AuthenticationController from "./routes/controllers/Authentication.controller"
 import CinemaHall from "./routes/controllers/CinemaHall.controller"
 import FilmController from "./routes/controllers/Film.controller"
-import AuthenticationController from "./routes/controllers/authentication.controller"
-import FilmController from "./routes/controllers/film.controller"
 import PaymentController from "./routes/controllers/payment.controller"
 
 const app = new App([new AuthenticationController(), new FilmController(), new CinemaHall(), new PaymentController()])
