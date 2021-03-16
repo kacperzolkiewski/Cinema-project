8f86631c2f737984ab2954184d04be1e2bfbc0c7
Mateusz Cabała <mateusz.cabala95@gmail.com>
2021-03-16 18:49:46 +0100

    Update files name

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import App from "./App"
+import AuthenticationController from "./routes/controllers/Authentication.controller"
 import CinemaHall from "./routes/controllers/CinemaHall.controller"
-import AuthenticationController from "./routes/controllers/authentication.controller"
-import FilmController from "./routes/controllers/film.controller"
+import FilmController from "./routes/controllers/Film.controller"
 
 const app = new App([new AuthenticationController(), new FilmController(), new CinemaHall()])
 
