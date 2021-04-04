269b0839a562931f8c0550c64130fc7740e80807
Karolina Gutka <karolina.gutka.97@gmail.com>
2021-04-04 12:39:24 +0200

    Add triangle to premiere div and layer over movieContainer

--- frontend/src/components/MovieCard/MovieCard.tsx
+++ frontend/src/components/MovieCard/MovieCard.tsx
@@ -1,4 +1,4 @@
-import { blueGrey } from "@material-ui/core/colors"
+import blue from "@material-ui/core/colors/blue"
 import { makeStyles } from "@material-ui/core/styles"
 import { specialColor } from "../design/system/colors/colors"
 
@@ -17,17 +17,27 @@ const useStyles = makeStyles(() => ({
     maxWidth: "120px",
     marginRight: "auto"
   },
+  layer: {},
   movieContainer: {
     padding: "5px"
   },
   premiere: {
-    padding: "1px 6px",
+    display: "flex",
     maxWidth: "90px",
-    fontSize: "0.6rem",
-    // eslint-disable-next-line
-    background: `linear-gradient(to right, ${specialColor} 80%, ${blueGrey[900]})`,
+    fontSize: "0.5rem",
     position: "absolute",
     textTransform: "uppercase"
+  },
+  rectangle: {
+    padding: "1px 4px 0 4px",
+    background: `linear-gradient(to right, ${specialColor} 50%, ${blue[900]})`
+  },
+  triangle: {
+    margin: 0,
+    width: 0,
+    height: 0,
+    borderTop: `15px solid ${blue[900]}`,
+    borderRight: "3px solid transparent"
   },
   movieImg: {
     maxHeight: 180,
@@ -50,13 +60,20 @@ function MovieCard(props: IMovieCard) {
   const classes = useStyles()
   return (
     <div className={classes.container}>
-      <div className={classes.movieContainer}>
-        <div className={props.isPremiere ? classes.premiere : classes.hidden}>
-          premiera
+      <div className={classes.layer}>
+        <div className={classes.movieContainer}>
+          <div className={props.isPremiere ? classes.premiere : classes.hidden}>
+            <div className={classes.rectangle}>premiera</div>
+            <div className={classes.triangle}></div>
+          </div>
+          <img
+            className={classes.movieImg}
+            alt="Movie"
+            src={props.imageSource}
+          />
         </div>
-        <img className={classes.movieImg} alt="Movie" src={props.imageSource} />
+        <div className={classes.movieTitle}>{props.movieTitle}</div>
       </div>
-      <div className={classes.movieTitle}>{props.movieTitle}</div>
     </div>
   )
 }
