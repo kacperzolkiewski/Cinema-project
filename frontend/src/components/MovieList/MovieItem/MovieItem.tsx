e4b18ee9e7b955538733a88a717e426ddea4de00
Mateusz Cabała <mateusz.cabala95@gmail.com>
2021-04-06 22:37:52 +0200

    Finish ShowingCard

--- frontend/src/components/MovieList/MovieItem/MovieItem.tsx
+++ frontend/src/components/MovieList/MovieItem/MovieItem.tsx
@@ -1,6 +1,7 @@
 import { ListItem } from "@material-ui/core"
 import React from "react"
 import IMovie from "../../../common/interfaces/Movies/Movie.interface"
+import ShowingCard from "../ShowingCard/ShowingCard"
 import styles from "./MovieItem.module.css"
 
 const MovieItem: React.FC<IMovie> = (props: Partial<IMovie>): JSX.Element => {
@@ -11,6 +12,11 @@ const MovieItem: React.FC<IMovie> = (props: Partial<IMovie>): JSX.Element => {
         {props.type} | {props.ageCategory} | {props.filmLength}
       </p>
       <p>{props.mainActors}</p>
+      <ShowingCard
+        key={props._id}
+        hours={props.hours as string[]}
+        is3D={props.is3D as boolean}
+      />
     </ListItem>
   )
 }
--- frontend/src/components/MovieList/ShowingCard/ShowingCard.tsx
+++ frontend/src/components/MovieList/ShowingCard/ShowingCard.tsx
@@ -1,10 +1,47 @@
-import { Card } from "@material-ui/core"
+import { Card, CardContent, Typography } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core"
 
+const useStyles = makeStyles(() => ({
+  root: {
+    width: 140,
+    height: 80,
+    display: "flex",
+    justifyContent: "center",
+    alignContent: "center",
+    backgroundColor: "transparent",
+    color: "white",
+    border: "2px solid #6E093A"
+  },
+  textCenter: {
+    textAlign: "center"
+  }
+}))
 const ShowingCard = (props: {
   hours: string[]
-  dates: string[]
+  is3D: boolean
 }): JSX.Element => {
-  return <Card></Card>
+  const classes = useStyles()
+
+  return (
+    <div>
+      {props.hours!.map((h, i) => (
+        <Card key={i} className={classes.root}>
+          <CardContent>
+            <Typography variant={"h6"} key={i} className={classes.textCenter}>
+              {h}
+            </Typography>
+            <Typography
+              variant={"subtitle1"}
+              key={i - 1}
+              className={classes.textCenter}
+            >
+              {props.is3D ? "3D" : "2D"}
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  )
 }
 
 export default ShowingCard
